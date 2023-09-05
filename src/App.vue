09044e0595f971be452c3742bea2fbdbb989a9f3
thephoo2546 <thephoo7@gmail.com>
2023-09-05 13:27:06 +0700

    game project

---
+++ src/App.vue
@@ -0,0 +1,146 @@
+<script setup>
+import { ref } from 'vue';
+
+const RightChoice = ref('');
+const result = ref('');
+const LeftChoice = ref('');
+const leftScore = ref(0);
+const rightScore = ref(0);
+
+function play() {
+  const choices = ['ค้อน', 'กระดาษ', 'กรรไกร'];
+  const randomIndex = Math.floor(Math.random() * 3);
+  const Right = choices[randomIndex];
+  
+  const LeftRandomIndex = Math.floor(Math.random() * 3);
+  const Left = choices[LeftRandomIndex];
+
+  RightChoice.value = Right;
+  LeftChoice.value = Left;
+  result.value = ''; 
+
+  
+  if (Left === Right) {
+    result.value = "เสมอ";
+  } else if (
+    (Left === 'ค้อน' && Right === 'กรรไกร') ||
+    (Left === 'กระดาษ' && Right === 'ค้อน') ||
+    (Left === 'กรรไกร' && Right === 'กระดาษ')
+  ) {
+    result.value = 'ฝั่งซ้ายชนะ !!!!';
+    leftScore.value += 1; 
+  } else {
+    result.value = 'ฝั่งขวาชนะ !!!!';
+    rightScore.value += 1; 
+  }
+}
+
+function reset() {
+  RightChoice.value = '';
+  result.value = '';
+  LeftChoice.value = '';
+  leftScore.value = 0; 
+  rightScore.value = 0; 
+}
+</script>
+
+
+<template>
+  <div>
+    <h2 class="head">เกม เป่ายิงฉุบ By Piriya</h2>
+
+    <button @click="play()" class="play-button">เล่น</button>
+    <button @click="reset()" class="reset-button">Reset</button>
+
+    <div class="play-container">
+      <div class="Left-choice">
+        <h2>ฝั่งซ้าย</h2>
+        <p>ผลลัพธ์ คือ : <b>{{ LeftChoice }}</b></p>
+        <img v-if="LeftChoice === 'ค้อน'" src="./assets/rock.png" alt="ค้อน">
+        <img v-if="LeftChoice === 'กระดาษ'" src="./assets/paper.png" alt="กระดาษ">
+        <img v-if="LeftChoice === 'กรรไกร'" src="./assets/scissors.png" alt="กรรไกร">
+      </div>
+
+      <div class="Right-choice">
+        <h2>ฝั่งขวา</h2>
+        <p>ผลลัพธ์ คือ : <b>{{ RightChoice }}</b></p>
+        <img v-if="RightChoice === 'ค้อน'" src="./assets/rock.png" alt="ค้อน">
+        <img v-if="RightChoice === 'กระดาษ'" src="./assets/paper.png" alt="กระดาษ">
+        <img v-if="RightChoice === 'กรรไกร'" src="./assets/scissors.png" alt="กรรไกร">
+      </div>
+    </div>
+
+    <p class="result">ผลลัพธ์: <b>{{ result }}</b></p>
+    <p class="score">ฝั่งซ้าย <b>{{ leftScore }}</b> คะแนน : ฝั่งขวา <b>{{ rightScore }}</b> คะแนน</p>
+  </div>
+</template>
+
+
+<style>
+button {
+  margin: 10px;
+}
+
+button.play-button{
+  background-color: rgb(24, 201, 24);
+  color: black;
+}
+
+button.play-button:hover{
+  background-color: green;
+  color: black;
+}
+
+button.reset-button {
+  background-color: red;
+  color: black;
+}
+
+button.reset-button:hover{
+  background-color: rgb(134, 20, 20);
+  color: black;
+}
+
+.head{
+  font-size: 42px;
+  color: brown;
+}
+
+.play-container {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 20px;
+}
+
+.Left-choice,
+.Right-choice {
+  text-align: center;
+  width: 50%;
+}
+
+.Left-choice h2,
+.Right-choice h2 {
+  font-size: 20px;
+  margin-bottom: 10px;
+}
+
+.Left-choice img,
+.Right-choice img {
+  max-width: 100%;
+  max-height: 400px;
+  height: auto;
+  display: block;
+  margin: 0 auto; 
+}
+
+.result {
+  text-align: center;
+  font-size: 42px;
+  margin-top: 20px;
+  color: red;
+}
+
+.score{
+  font-size: 38px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+let name = ref('Piriya')
+
+let isOn = ref(false)
+const statusText = ref('ปิด')
+
+const toggleButtonState = () => {
+  isOn.value = !isOn.value;
+  statusText.value = isOn.value ? 'เปิด' : 'ปิด';
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <h2>{{ name }}</h2>
+
+  <div class="card">
+    <button id="toggleButton" @click="toggleButtonState" :class="{ 'red-button': isOn }">ON/OFF</button>
+    <p id="status">{{ statusText }}</p>
+
+    <button type="button" @click="count++" style="margin-top: 10px;" >count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+
+h2{
+  color: orange;
+  font-size: 40px;
+}
+
+h1{
+  color: blue;
+  font-size: 40px;
+}
+.red-button {
+  background-color: red;
+  color: white;
+}
+</style>
